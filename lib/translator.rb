035d9ffc8247622afdadf9c5778a5c4f7f9678cb
SANJAY K SAI <shachsan@hotmail.com>
2018-10-26 13:09:22 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,14 +5,10 @@ require 'pry'
 def load_library(file_path)
 
   emo = YAML.load_file(file_path)
-  # binding.pry
   translate_emoticons ={"get_emoticon" => {}, "get_meaning" => {}}
-  # new_hash={}
   emo.each do |eng, emoticons|
     translate_emoticons["get_meaning"][emoticons[1]]=eng
     translate_emoticons["get_emoticon"][emoticons[0]]=emoticons[1]
-
-      # binding.pry
   end
   translate_emoticons
 
