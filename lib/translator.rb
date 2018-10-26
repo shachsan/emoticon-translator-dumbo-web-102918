16fda92119d0a8eff357d6227e5cba5fec82b8c1
SANJAY K SAI <shachsan@hotmail.com>
2018-10-26 13:30:21 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file_path)
   emo.each do |eng_name, emoticons|
     eng_emot, jap_emot = emoticons
     translate_emoticons["get_meaning"][emoticons[jap_emot]]=eng_name
-    translate_emoticons["get_emoticon"][emoticons[eng_emot]]=emoticons[jap_emot]
+    translate_emoticons["get_emoticon"][emoticons[eng_emot]]=jap_emot
   end
   translate_emoticons
 
